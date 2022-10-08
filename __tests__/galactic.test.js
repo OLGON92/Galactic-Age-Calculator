c346702b3b24fde2f18f3a879825225a82e1ea15
OLGON92 <ogon318@gmail.com>
2022-10-08 14:08:55 -0700

    entering red phase of mercYrsLeft

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -33,11 +33,15 @@ describe('GalacticCalc', () => {
     expect(age.jupAge()).toEqual(2);
   });
 
-  test('should give the users life expectancy in earth years in the form of a string', () => {
+  test('should give the users life expectancy in Earth years in the form of a string', () => {
     expect(age.earthYrsLeft()).toEqual("You have 49 earth years left to live!");
   });
 
-  test('should give the users number of earth years that they have lived past their life expectancy in the form of a string', () => {
+  test('should give the users number of Earth years that they have lived past their life expectancy in the form of a string', () => {
     expect(age2.earthYrsLeft()).toEqual("You have lived 11 earth years past your life expectancy!");
+  });
+
+  test('should give the users life expectancy in Mercury years in the form of a string', () => {
+    expect(age.mercYrsLeft()).toEqual("You have 204 mercury years left to live!");
   });
 });
