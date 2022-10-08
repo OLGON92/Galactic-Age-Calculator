6687f142661b8bd70598e20512be5a4d556226ab
OLGON92 <ogon318@gmail.com>
2022-10-08 13:06:13 -0700

    conduct second test on earthYrsLeft to get years lived pas expectancy

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,9 +2,15 @@ import GalacticCalc from '../src/js/galactic.js';
 
 describe('GalacticCalc', () => {
   let age;
+  let age2;
 
   beforeEach(() => {
     age = new GalacticCalc(30);
+  });
+
+
+  beforeEach(() => {
+    age2 = new GalacticCalc(90);
   });
 
   test('should correctly create a GalacticCalc object based on users age', () => {
@@ -29,5 +35,9 @@ describe('GalacticCalc', () => {
 
   test('should give the users life expectancy in earth years in the form of a string', () => {
     expect(age.earthYrsLeft()).toEqual("You have 49 earth years left to live!");
+  });
+
+  test('should give the users number of earth years that they have lived past their life expectancy in the form of a string', () => {
+    expect(age2.earthYrsLeft()).toEqual("You have lived 11 earth years past your life expectancy!");
   });
 });
